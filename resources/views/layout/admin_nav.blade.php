09b910321822f72f5d910305c376e27474c42656
shivangi-singh03 <49403357+shivangi-singh03@users.noreply.github.com>
2022-04-25 18:53:28 +0530

    Add files via upload

---
+++ resources/views/layout/admin_nav.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.app')
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/panel">Book Details</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/student">Student Details <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+</div>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="api/details">Student Info <span class="sr-only">(current)</span></a>
+      </li>
+      </ul>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/logout">Logout <span class="sr-only">(current)</span></a>
+      </li>
+      </ul>
+    </div>
+</nav>
+
+
