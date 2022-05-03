2af3ed73879bee4d1026ef7f50436ba3be571a98
shivangi-singh03 <49403357+shivangi-singh03@users.noreply.github.com>
2022-05-03 17:54:52 +0530

    Add files via upload

---
+++ resources/views/student/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.app')
+        <form action="{{route('student_update',[$student->id])}}" method="post">
+        {{csrf_field()}}
+        {{ method_field('PUT') }}
+         <div class="container">
+          <h1 class="text-center">Edit Student Details</h1>
+           <div class="form-group">
+               <label for="Name">Name</label>
+               <input type="text" name="name" id="" class="form-control" placeholder="" required value="{{$student->name}}"/>
+            </div>
+
+            <div class="form-group">
+               <label for="Email">Email</label>
+               <input type="email" name="email" id="" class="form-control" placeholder="" required value="{{$student->email}}"/>
+            </div>
+
+            <div class="form-group">
+               <label for="Password">Password</label>
+               <input type="password" name="password" id="" class="form-control" placeholder="" required value="{{$student->password}}"/>
+            </div>
+
+            <button class="btn-btn-primary">
+             Save
+            </button>
+
+            
+         </div>
+        
+
